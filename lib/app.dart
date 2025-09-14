b7707c0e60506016b542119e242da5f6c2d81f15
Mohamed Heiba <mohamed222101223@ksiu.edu.eg>
2025-09-14 18:14:56 +0300

    add Appcolors for my app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:the_magnificent_three/core/constants/app_colors.dart';
+import 'package:the_magnificent_three/presentation/pages/auth/auth_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        colorScheme: ColorScheme.light(
+          brightness: Brightness.light,
+          primary: AppColors.primaryLight,
+          onPrimary: AppColors.onPrimaryLight,
+          error: AppColors.errorLight,
+          onError: AppColors.onErrorLight,
+          surface: AppColors.surfaceLight,
+          onSurface: AppColors.onSurfaceLight,
+          background: AppColors.backgroundLight,
+          onBackground: AppColors.onBackgroundLight,
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        colorScheme: const ColorScheme.dark(
+          brightness: Brightness.dark,
+          primary: AppColors.primaryDark,
+          onPrimary: AppColors.onPrimaryDark,
+          error: AppColors.errorDark,
+          onError: AppColors.onErrorDark,
+          surface: AppColors.surfaceDark,
+          onSurface: AppColors.onSurfaceDark,
+          background: AppColors.backgroundDark,
+          onBackground: AppColors.onBackgroundDark,
+        ),
+      ),
+      home: AuthPage(),
+    );
   }
 }
